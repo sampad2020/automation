653237edc4cf2b2dc36a9bf46caa35b850d5ba00
SAIBABA <SAIBABA@SAIBABA-PC>
2014-11-27 17:12:56 +0530

    Test Case updated

---
+++ src/selenim/NewTest.java
@@ -0,0 +1,12 @@
+package selenim;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	WebDriver driver;
+  @Test
+  public void f() {
+	  System.out.println("demo");
+  }
+}
